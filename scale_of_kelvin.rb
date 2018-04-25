98f6b62704c7baa3be9ba4e97f0ee69dcf8283dc
Paúl <34814922+PaulMaldonado@users.noreply.github.com>
2018-04-24 22:03:38 -0500

    Create scale_of_kelvin.rb
    Programa que calcula la temperatura termodinámica.

---
+++ scale_of_kelvin.rb
@@ -0,0 +1,7 @@
+KELVIN = 294
+CELSIUS = KELVIN - 273
+FAHRENHEIT = CELSIUS * (9 / 5) + 32
+
+puts "La temperatura es #{FAHRENHEIT}"
+
+# La escala de Kelvin es una escala de temperatura termodinámica.
